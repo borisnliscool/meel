ebc0cc099029b2d3b12c46db14051738355c2b9a
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2024-06-28 18:40:09 +0200

    chore: create render template route

--- src/routes/templates.rs
+++ src/routes/templates.rs
@@ -1,6 +1,11 @@
+use std::collections::HashMap;
+
+use axum::extract::Path;
 use axum::http::StatusCode;
 use axum::Json;
 use serde::Serialize;
+
+use crate::templating;
 
 #[derive(Serialize)]
 pub struct Template {
@@ -10,4 +15,11 @@ pub struct Template {
 pub async fn get_templates() -> Result<Json<Vec<Template>>, StatusCode> {
     // TODO: Implement fetching templates
     Ok(Json(vec![]))
+}
+
+pub async fn render_template(Path(template_name): Path<String>, Json(data): Json<HashMap<String, String>>) -> Result<String, StatusCode> {
+    match templating::render(template_name, data) {
+        Ok(html) => Ok(html),
+        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -3,12 +3,13 @@ use axum::routing::{get, post};
 use tower_http::trace::TraceLayer;
 
 use crate::routes::mails::{get_mail_status, send_mails};
-use crate::routes::templates::get_templates;
+use crate::routes::templates::{get_templates, render_template};
 
 pub async fn create() -> Router {
     Router::new()
         .layer(TraceLayer::new_for_http())
-        .route("/mail/send", post(send_mails))
-        .route("/mail/:mail_id", get(get_mail_status))
+        .route("/mails/send", post(send_mails))
+        .route("/mails/:mail_id", get(get_mail_status))
         .route("/templates", get(get_templates))
+        .route("/templates/:template_name/render", post(render_template))
 }
